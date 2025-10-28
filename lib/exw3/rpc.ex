d5d8d7e1c02df29523d1317499dbab91c8bbfe2a
Steve Albrechtsen <stevenalbrechtsen@gmail.com>
2025-10-28 01:40:48 -0600

    opts please

--- lib/exw3/rpc.ex
+++ lib/exw3/rpc.ex
@@ -174,7 +174,7 @@ defmodule ExW3.Rpc do
   @doc "Simple eth_call to client. Recommended to use ExW3.Contract.call instead."
   @spec eth_call(list()) :: any()
   def eth_call(arguments, opts \\ []) do
-    call_client(:eth_call, [arguments, opts])
+    call_client(:eth_call, arguments ++ [opts])
   end
 
   @doc "Simple eth_send_transaction. Recommended to use ExW3.Contract.send instead."
