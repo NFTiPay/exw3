7b40019ec5c28fbc14f8997e8ebf32a118235dff
Steve Albrechtsen <stevenalbrechtsen@gmail.com>
2025-10-28 01:11:38 -0600

    revert

--- lib/exw3/contract.ex
+++ lib/exw3/contract.ex
@@ -39,8 +39,8 @@ defmodule ExW3.Contract do
 
   @doc "Use a Contract's method with an eth_call"
   @spec call(atom(), atom(), list(), any()) :: {:ok, any()}
-  def call(contract_name, method_name, args \\ [], options \\ [], timeout \\ :infinity) do
-    GenServer.call(ContractManager, {:call, {contract_name, method_name, args, options}}, timeout)
+  def call(contract_name, method_name, args \\ [], timeout \\ :infinity) do
+    GenServer.call(ContractManager, {:call, {contract_name, method_name, args}}, timeout)
   end
 
   @doc "Use a Contract's method with an eth_sendTransaction"
@@ -51,8 +51,8 @@ defmodule ExW3.Contract do
 
   @doc "Returns a formatted transaction receipt for the given transaction hash(id)"
   @spec tx_receipt(atom(), binary()) :: map()
-  def tx_receipt(contract_name, tx_hash, options \\ []) do
-    GenServer.call(ContractManager, {:tx_receipt, {contract_name, tx_hash, options}})
+  def tx_receipt(contract_name, tx_hash) do
+    GenServer.call(ContractManager, {:tx_receipt, {contract_name, tx_hash}})
   end
 
   @doc "Installs a filter on the Ethereum node. This also formats the parameters, and saves relevant information to format event logs."
@@ -200,14 +200,13 @@ defmodule ExW3.Contract do
     {tx_receipt["contractAddress"], tx_hash}
   end
 
-  def eth_call_helper(address, abi, method_name, args, options \\ []) do
+  def eth_call_helper(address, abi, method_name, args) do
     result =
       ExW3.Rpc.eth_call([
         %{
           to: address,
           data: "0x#{ExW3.Abi.encode_method_call(abi, method_name, args)}"
-        },
-        options
+        }
       ])
 
     case result do
@@ -474,11 +473,11 @@ defmodule ExW3.Contract do
     {:reply, state[name][:address], state}
   end
 
-  def handle_call({:call, {contract_name, method_name, args, options}}, _from, state) do
+  def handle_call({:call, {contract_name, method_name, args}}, _from, state) do
     contract_info = state[contract_name]
 
     with {:ok, address} <- check_option(contract_info[:address], :missing_address) do
-      result = eth_call_helper(address, contract_info[:abi], Atom.to_string(method_name), args, options)
+      result = eth_call_helper(address, contract_info[:abi], Atom.to_string(method_name), args)
       {:reply, result, state}
     else
       err -> {:reply, err, state}
@@ -506,10 +505,10 @@ defmodule ExW3.Contract do
     end
   end
 
-  def handle_call({:tx_receipt, {contract_name, tx_hash, options}}, _from, state) do
+  def handle_call({:tx_receipt, {contract_name, tx_hash}}, _from, state) do
     contract_info = state[contract_name]
 
-    case ExW3.tx_receipt(tx_hash, options) do
+    case ExW3.tx_receipt(tx_hash) do
       {:ok, receipt} ->
         events = contract_info[:events]
         logs = receipt["logs"]
